3274b17f413c7b17d09f3e969497561182665829
Harish Kumar S P <harishkumar.sp5511@gmail.com>
2025-09-20 16:05:08 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDkab7UpZ4ZIvAt9c8v8yd1v2dnbrOBlI8",
-  authDomain: "studio-8680828960-28e64.firebaseapp.com",
-  projectId: "studio-8680828960-28e64",
-  storageBucket: "studio-8680828960-28e64.appspot.com",
-  messagingSenderId: "1010855061710",
-  appId: "1:1010855061710:web:ed8625cbe51468ed86089f",
+  apiKey: 'AIzaSyDkab7UpZ4ZIvAt9c8v8yd1v2dnbrOBlI8',
+  authDomain: 'studio-8680828960-28e64.firebaseapp.com',
+  projectId: 'studio-8680828960-28e64',
+  storageBucket: 'studio-8680828960-28e64.appspot.com',
+  messagingSenderId: '1010855061710',
+  appId: '1:1010855061710:web:ed8625cbe51468ed86089f',
 };
 
 // Initialize Firebase
