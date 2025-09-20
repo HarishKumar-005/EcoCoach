03442e98617cd98a6d4d9b040e1e40fecabf4590
Harish Kumar S P <harishkumar.sp5511@gmail.com>
2025-09-20 15:57:16 +0000

    The requested action is invalid.
    the sign in is not working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased" suppressHydrationWarning>
+      <body className="font-body antialiased" suppressHydrationWarning={true}>
         <AuthProvider>
           {children}
           <Toaster />
